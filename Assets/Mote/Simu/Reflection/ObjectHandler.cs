cb60577303e0feddc47280b529937f9631945194
J. Santos <jamillo@gmail.com>
2016-09-02 00:58:43 -0300

    Object properties manipulation through the property name

---
+++ Assets/Mote/Simu/Reflection/ObjectHandler.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Mote.Simu.Reflection
+{
+	public abstract class ObjectHandler
+	{
+		public static void SetProperty(object instance, string property, object value)
+		{
+			instance.GetType().GetProperty(property).SetValue(instance, value, null);
+		}
+	}
+}
