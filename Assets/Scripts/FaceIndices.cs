ee621efbaa75dab96cec1a85dfabe1b041e1a27f
J. Santos <jamillo@gmail.com>
2016-09-02 01:13:02 -0300

    Adds code from a third part for importing OBJ files into Unity scene in runtime

---
+++ Assets/Scripts/FaceIndices.cs
@@ -0,0 +1,12 @@
+using System;
+
+/// <summary>
+/// This code was adapted from https://github.com/hammmm/unity-obj-loader, check the URL for more information
+/// about the author and LICENSE.
+/// </summary>
+public struct FaceIndices
+{
+	public int vi;
+	public int vu;
+	public int vn;
+}
